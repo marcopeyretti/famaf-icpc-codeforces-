4f7e053dd5711c037cb051c09b69172d1e02cf25
marcopeyretti <166165529+marcopeyretti@users.noreply.github.com>
2024-04-17 14:33:31 -0300

    Update x.cpp

--- Resoluciones_Codeforces/x.cpp
+++ Resoluciones_Codeforces/x.cpp
@@ -6,5 +6,14 @@ using namespace std;
 int main ()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-        
+    vector <int> v;
+    v.push_back(3);
+    v.pop_back();
+    v.clear();
+    for (int num : miVector) 
+    {
+        std::cout << num << " ";
+    }
+    sort(v.begin(),v.end(),cont);
+    v.erase(v.begin());
 }
